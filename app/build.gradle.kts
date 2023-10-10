1ce9d96fb64a6e0b0b604a7156aeab645ceb793f
CreationCollection <creationcollection011@gmail.com>
2023-10-10 21:10:06 +0530

    Feat: Add OkHttp Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
 
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
 
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")

3bee036924880e91f7f68d03479028a0ea7d511b
CreationCollection <creationcollection011@gmail.com>
2023-10-14 10:39:59 +0530

    Update: Create new file for utils functions

--- app/src/main/java/com/redline/anistalker/ui/components/uiUtils.kt
+++ app/src/main/java/com/redline/anistalker/ui/components/uiUtils.kt
@@ -1,12 +1,13 @@
 package com.redline.anistalker.ui.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 
@@ -26,9 +27,9 @@ fun CenteredBox(
 
 // region Helper Functions
 @Composable
-fun awarePainterResource(resId: Int, color: Color = Color(0xFF151515)): Painter {
-    return if (LocalInspectionMode.current) ColorPainter(color)
+fun awarePainterResource(resId: Int): Painter {
+    return if (LocalInspectionMode.current) rememberVectorPainter(image = Icons.Filled.Add)
     else painterResource(id = resId)
 }
 
-private fun lerp(start: Float, end: Float, t: Float) = start + (end - start) * t
+fun lerp(start: Float, end: Float, t: Float) = start + (end - start) * t

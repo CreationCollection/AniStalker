a86beafa54bd133b787c62e72190df2391546e1e
CreationCollection <creationcollection011@gmail.com>
2023-10-25 15:56:28 +0530

    Update: Update Gradle Build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.paging:paging-common-android:3.3.0-alpha02")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     val media3_version = "1.1.1"
     val lifecycle_version = "2.6.2"
@@ -66,22 +67,24 @@ dependencies {
     implementation ("com.google.android.gms:play-services-auth:20.7.0")
 
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
+    implementation ("androidx.navigation:navigation-compose:2.7.4")
 
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }

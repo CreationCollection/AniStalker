878363b70d80db1ab9f563868e06e2f256ecf928
CreationCollection <creationcollection011@gmail.com>
2023-10-29 15:37:31 +0530

    Feat: Add Bitmap Util functions

---
+++ app/src/main/java/com/redline/anistalker/utils/BitmapUtils.kt
@@ -0,0 +1,33 @@
+@file:Suppress("DEPRECATION")
+
+package com.redline.anistalker.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.renderscript.Allocation
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
+
+fun Context.blurImage(image: Bitmap, radius: Float = 20f): Bitmap {
+    return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+        val rs = RenderScript.create(this)
+        val bitmapAlloc = Allocation.createFromBitmap(rs, image)
+        val output = Allocation.createTyped(rs, bitmapAlloc.type)
+        ScriptIntrinsicBlur.create(rs, bitmapAlloc.element).apply {
+            setRadius(radius)
+            setInput(bitmapAlloc)
+            forEach(output)
+        }
+        output.copyTo(image)
+        rs.destroy()
+        image
+    } else {
+        image
+    }
+}
+
+fun Context.blurImageResource(image: Int, radius: Float = 25f): Bitmap {
+    return blurImage(BitmapFactory.decodeResource(resources, image), radius)
+}

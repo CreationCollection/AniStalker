e56eb95b2d651d6e5084e33fbd38cd2dbbd2722f
CreationCollection <creationcollection011@gmail.com>
2023-10-31 15:36:43 +0530

    Feat: Add DownloadTask interface

---
+++ app/src/main/java/com/redline/anistalker/managements/downloadSystem/DownloadTask.kt
@@ -0,0 +1,21 @@
+package com.redline.anistalker.managements.downloadSystem
+
+import com.redline.anistalker.models.DownloadStatus
+
+interface DownloadTask {
+    val fileName: String
+    val episodeId: Int
+    val duration: Float
+
+    fun status(): DownloadStatus
+    fun downloadedSize(): Long
+    fun downloadedDuration(): Float
+    fun downloadSpeed(): Long
+
+    fun start()
+    fun stop()
+    fun cancel()
+    fun restart()
+
+    override fun toString(): String
+}

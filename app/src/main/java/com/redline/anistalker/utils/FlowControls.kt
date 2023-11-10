8620da7f5a4231815ce55953ac168f2903c53a84
CreationCollection <creationcollection011@gmail.com>
2023-11-10 14:58:40 +0530

    Update: Add await function in ExecutionFlow and add runWith extensions functions

--- app/src/main/java/com/redline/anistalker/utils/FlowControls.kt
+++ app/src/main/java/com/redline/anistalker/utils/FlowControls.kt
@@ -1,7 +1,9 @@
 package com.redline.anistalker.utils
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -11,24 +13,54 @@ class ExecutionFlow(
 ) {
     private val queue = ConcurrentLinkedQueue<suspend CoroutineScope.() -> Unit>()
     private val currentCount = AtomicInteger(0)
+    private val completionJob = Job()
 
     fun execute(callback: suspend CoroutineScope.() -> Unit) {
         queue.add(callback)
         operateTasks()
     }
 
-    fun isEmpty(): Boolean = queue.size == 0
+    fun isEmpty(): Boolean = queue.size == 0 && currentCount.get() <= 0
+
+    fun await() {
+        runBlocking { completionJob.join() }
+    }
 
     private fun operateTasks() {
         if (currentCount.get() < concurrentCount) queue.poll()?.let {
             currentCount.incrementAndGet()
 
             scope.launch {
-                it()
+                try { it() } catch (err: Exception) { err.printStackTrace() }
             }.invokeOnCompletion {
                 currentCount.decrementAndGet()
                 operateTasks()
+
+                if (isEmpty()) {
+                    completionJob.complete()
+                }
             }
         }
     }
+}
+
+
+fun<A, B> runWith(a: A, b: B, use: (a: A, b: B) -> Unit) {
+    use(a, b)
+}
+
+fun<A, B, C> runWith(a: A, b: B, c: C, use: (a: A, b: B, c: C) -> Unit) {
+    use(a, b, c)
+}
+
+fun<A, B, C, D> runWith(a: A, b: B, c: C, d: D, use: (a: A, b: B, c: C, d: D) -> Unit) {
+    use(a, b, c, d)
+}
+
+fun<A, B, C, D, E> runWith(a: A, b: B, c: C, d: D, e: E, use: (a: A, b: B, c: C, d: D, e: E) -> Unit) {
+    use(a, b, c, d, e)
+}
+
+fun<G> G.runWith(vararg values: Any, block: G.(values: Array<out Any>) -> Unit) {
+    block(values)
 }

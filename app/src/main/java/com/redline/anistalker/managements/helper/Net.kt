0dc98d0a159e399e023da1f6639fc62a85bff272
CreationCollection <creationcollection011@gmail.com>
2023-11-10 14:57:49 +0530

    Feat: Return content length on getStream method

--- app/src/main/java/com/redline/anistalker/managements/helper/Net.kt
+++ app/src/main/java/com/redline/anistalker/managements/helper/Net.kt
@@ -3,18 +3,20 @@ package com.redline.anistalker.managements.helper
 import android.graphics.BitmapFactory
 import com.redline.anistalker.models.AniError
 import com.redline.anistalker.models.AniErrorCode
-import okhttp3.*
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import java.io.IOException
 import java.io.InputStream
 import java.net.SocketTimeoutException
 import java.util.concurrent.Executors
-import javax.net.ssl.SSLException
 
 object Net {
     private val workPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
     private val client = OkHttpClient()
 
-    // HTTP GET request
     fun get(url: String): String {
         val request = Request.Builder()
             .url(url)
@@ -23,12 +25,17 @@ object Net {
         return requestForString(request)
     }
 
-    fun getStream(url: String): InputStream {
+    fun getStream(url: String, contentLength: ((Long) -> Unit)? = null): InputStream {
         val request = Request.Builder()
             .url(url)
             .build()
 
-        return executeRequest(request) { it?.byteStream() ?: throw IOException("Empty Body") }
+        return executeRequest(request, {
+            contentLength?.run {
+                val length = it["Content-Length"]?.toLongOrNull() ?: 0
+                this(length)
+            }
+        }) { it?.byteStream() ?: throw IOException("Empty Body") }
     }
 
     // HTTP POST request
@@ -63,13 +70,14 @@ object Net {
 
     fun cacheImage(url: String): Boolean {
         if (!url.startsWith("http://") &&
-            !url.startsWith("https://")) return false
+            !url.startsWith("https://")
+        ) return false
 
         workPool.execute {
             try {
                 val stream = getStream(url)
                 val bitmap = BitmapFactory.decodeStream(stream)
-            } catch(ex: IOException) {
+            } catch (ex: IOException) {
                 ex.printStackTrace()
             }
         }
@@ -83,19 +91,21 @@ object Net {
         }
     }
 
-    private fun<Result> executeRequest(request: Request, handle: (ResponseBody?) -> Result): Result {
+    private fun <Result> executeRequest(
+        request: Request,
+        headers: ((Headers) -> Unit)? = null,
+        handle: (ResponseBody?) -> Result
+    ): Result {
         try {
             val response = client.newCall(request).execute()
             if (response.isSuccessful) {
+                headers?.let { it(response.headers) }
                 return handle(response.body)
-            }
-            else throw IOException(response.code.toString())
-        }
-        catch (e: SocketTimeoutException) {
+            } else throw IOException(response.code.toString())
+        } catch (e: SocketTimeoutException) {
             val errorCode = AniErrorCode.SLOW_NETWORK_ERROR
             throw AniError(errorCode)
-        }
-        catch (e: IOException) {
+        } catch (e: IOException) {
             e.printStackTrace()
             val errorCode = AniErrorCode.UNKNOWN
             throw AniError(errorCode, e.message ?: errorCode.message)

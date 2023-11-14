1e111e7ab3ed84248b24b4096db32f5205e718fa
CreationCollection <creationcollection011@gmail.com>
2023-11-14 23:34:49 +0530

    Update (Main Activity): Remove backStack for navigation graph

--- app/src/main/java/com/redline/anistalker/activities/MainActivity.kt
+++ app/src/main/java/com/redline/anistalker/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.redline.anistalker.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.fadeIn
@@ -18,9 +19,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -29,6 +32,8 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -73,6 +78,34 @@ class MainActivity : ComponentActivity() {
                 mutableIntStateOf(0)
             }
 
+            navController.enableOnBackPressed(false)
+            BackHandler(true) {
+                if (navController.currentDestination?.route == screenHome) finish()
+                else {
+                    navController.navigate(screenHome) {
+                        popUpTo(navController.graph.findStartDestination().id) { saveState = true }
+                    }
+                }
+            }
+
+            val onNavChange = remember {
+                    NavController.OnDestinationChangedListener { _, destination, _ ->
+                        selectedNavBar = when (destination.route) {
+                            screenSearch -> 1
+                            screenLibrary -> 2
+                            screenDownload -> 3
+                            else -> 0
+                        }
+                    }
+                }
+            DisposableEffect(navController) {
+                navController.addOnDestinationChangedListener(onNavChange)
+                onDispose {
+                    navController.removeOnDestinationChangedListener(onNavChange)
+                }
+            }
+
+
             AniStalkerTheme {
                 Surface {
                     Scaffold(
@@ -89,13 +122,13 @@ class MainActivity : ComponentActivity() {
                                         3 -> screenDownload
                                         else -> screenHome
                                     }
-                                    navController.popBackStack(route, true)
-                                    navController.navigate(route)
-                                    selectedNavBar = it
+                                    navController.navigate(route) {
+                                        popUpTo(navController.graph.findStartDestination().id) { saveState = true }
+                                    }
                                 }
                             }
                         },
-                        contentWindowInsets = WindowInsets(0, 0, 0, 0)
+                        contentWindowInsets = WindowInsets(0)
                     ) { pads ->
                         Box(
                             modifier = Modifier

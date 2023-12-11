22bcae234e6d8dfb145575fac40f277ebb8945a1
Creation Collection <creationcollection011@gmail.com>
2023-12-11 17:39:49 +0530

    Update (Theme): Set false to LightStatusBars

--- app/src/main/java/com/redline/anistalker/ui/theme/Theme.kt
+++ app/src/main/java/com/redline/anistalker/ui/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -60,7 +59,7 @@ fun AniStalkerTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 

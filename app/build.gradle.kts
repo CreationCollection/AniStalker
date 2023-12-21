71d9021302954d58fbe8a440ecca15eb5fcc4621
Creation Collection <creationcollection011@gmail.com>
2023-12-21 20:40:48 +0530

    Update (Build.grade): Add Fragment library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
 
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
